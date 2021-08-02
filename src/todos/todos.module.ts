d4f094891610089329db607f50f780cf5ee4d997
riley909 <increaseblue666@gmail.com>
2021-08-02 20:02:41 +0900

    add TodosController, TodosService

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TodosController } from './todos.controller';
+import { TodosService } from './todos.service';
 
-@Module({})
+@Module({
+  controllers: [TodosController],
+  providers: [TodosService]
+})
 export class TodosModule {}
