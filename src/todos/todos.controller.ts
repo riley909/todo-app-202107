cbae58b13d756ce43dc4b5df69aae7dbed28b899
riley909 <increaseblue666@gmail.com>
2021-08-02 23:47:51 +0900

    add getTodoById

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Todo } from './todo.entity';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private todosService: TodosService) {}
+
+  @Get('/:id')
+  getTodoById(@Param('id') id: number): Promise<Todo> {
+    return this.todosService.getTodoById(id);
+  }
+}
