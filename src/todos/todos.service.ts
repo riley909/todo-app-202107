6346f138cea4de8ea8440227a06860f7b4ef5030
riley909 <increaseblue666@gmail.com>
2021-08-03 12:14:21 +0900

    add createTodo

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
 import { Todo } from './todo.entity';
 
 @Injectable()
@@ -9,5 +10,29 @@ export class TodosService {
       throw new NotFoundException(`id${id} not found`);
     }
     return found;
+  }
+
+  getDateFormat(date) {
+    const year = date.getFullYear();
+    let month = 1 + date.getMonth();
+    month = month >= 10 ? month : '0' + month;
+    let day = date.getDate();
+    day = day >= 10 ? day : '0' + day;
+    return `${year}-${month}-${day}`;
+  }
+
+  async createTodo(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const { content, ref } = createTodoDto;
+    const date = this.getDateFormat(new Date());
+    const todo = Todo.create({
+      content,
+      created: date,
+      lastEdited: date,
+      ref,
+      status: 0,
+    });
+
+    await Todo.save(todo);
+    return todo;
   }
 }
