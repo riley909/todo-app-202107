641b8411bd217fab826b954d25ef8ee561437122
riley909 <increaseblue666@gmail.com>
2021-08-05 13:57:54 +0900

    add validator

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,6 +1,7 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateTodoDto {
+  @IsNotEmpty()
   @IsString()
   content: string;
 
