e1eb96215a33f3c5e93f3dc4bf067fbcc532f545
riley909 <increaseblue666@gmail.com>
2021-08-05 14:34:38 +0900

    add todosRepository

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Todo } from './todo.entity';
 import { TodosController } from './todos.controller';
+import { TodosRepository } from './todos.repository';
 import { TodosService } from './todos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Todo])],
+  imports: [TypeOrmModule.forFeature([TodosRepository])],
   controllers: [TodosController],
   providers: [TodosService],
 })
